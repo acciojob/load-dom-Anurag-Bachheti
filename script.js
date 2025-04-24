81201351bbcf6ec6cba3ab80fd52da55640a97c9
Anurag Bachheti <157354714+Anurag-Bachheti@users.noreply.github.com>
2025-04-24 22:30:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-window.onload = function() {
-	const overlay = document.createElement("div");
-	overlay.className = "overlay-message";
-	overlay.innerText = "DOM load success"
-	document.documentElement.appendChild(overlay)
-};
+window.onload = function () {
+        document.body.innerHTML = "";
+
+        let overlay = document.createElement("div");
+        overlay.className = "overlay-message";
+        overlay.innerText = "DOM load success";
+
+        document.body.appendChild(overlay);
+      };
